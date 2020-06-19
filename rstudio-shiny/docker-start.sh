1344ede1aa4b1a561fc2025a18b9b210b9f5d9d2
Ben Best <ben@ecoquants.com>
2020-06-18 19:05:16 -0700

    cleanup

--- rstudio-shiny/docker-start.sh
+++
@@ -1,6 +0,0 @@
-# install Shiny apps
-RUN mkdir /share/github && cd /share/github && \
-  git clone https://github.com/marinebon/iea-uploader.git && \
-  ln -s iea-uploader /srv/shiny-server/uploader && \
-  git clone https://github.com/marinebon/edna-vis.git && \
-  ln -s shiny /srv/shiny-server/edna-vis
