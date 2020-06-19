364658eb24a735f25c37a799ebe4ef3795fa1191
Ben Best <ben@ecoquants.com>
2020-06-18 18:07:02 -0700

    shell commands to check for directories

--- nginx/docker-start.sh
+++ nginx/docker-start.sh
@@ -1,6 +1,10 @@
-mkdir /share/github
+[ ! -d "/share/github" ] && mkdir /share/github
 cd /share/github
 
-git clone https://github.com/marinebon/iea-ne_info.git
-mv /usr/share/nginx/html /usr/share/nginx/html_0
-ln -s iea-ne_info /usr/share/nginx/html
+repo=iea-ne_info
+if [ ! -d "/share/github/$repo" ]
+then 
+  git clone https://github.com/marinebon/$repo.git
+  mv /usr/share/nginx/html /usr/share/nginx/html_0
+  ln -s iea-ne_info /usr/share/nginx/html
+fi
