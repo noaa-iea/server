af65bca1e65a0eb66d3de48d21039db119b2ed19
Ben Best <ben@ecoquants.com>
2020-06-18 17:56:30 -0700

    RUN to sh

--- nginx/docker-start.sh
+++ nginx/docker-start.sh
@@ -1,6 +1,8 @@
-# install infographic site from Github
-RUN apt-get update; apt-get --assume-yes install git
-RUN mkdir /share/github && cd /share/github && \
-  git clone https://github.com/marinebon/iea-ne_info.git \
-  mv /usr/share/nginx/html /usr/share/nginx/html_0 \
-  ln -s iea-ne_info /usr/share/nginx/html
+apt-get update; apt-get --assume-yes install git
+
+mkdir /share/github
+cd /share/github
+
+git clone https://github.com/marinebon/iea-ne_info.git
+mv /usr/share/nginx/html /usr/share/nginx/html_0
+ln -s iea-ne_info /usr/share/nginx/html
